addbe988ba535188284f0c9f4fb6da77c69bbbe0
Emirhan Buğdaycı <148485417+Emirhanbgdyc@users.noreply.github.com>
2024-12-17 17:58:21 +0300

    Hero bölümündeki resimler aktarıldı

--- js/app.js
+++ js/app.js
@@ -1,14 +1,40 @@
 const homeButton =document.querySelector('.hero__btn');
 
-
+//!Buttona ozellik ekleme bolumu
 
 const runEvents = () =>{
     homeButton.addEventListener('mouseover',overEvents);
+    homeButton.addEventListener('mouseout',outEvents);
 }
 
 
 const overEvents = () =>{
-    homeButton.style.backgroundColor ='#ffffff';
+    homeButton.style.transform = 'scale(1.1)';
 };
 
+const outEvents =() =>{
+    homeButton.style.transform = 'scale(1)';
+}
+
 runEvents();
+
+//! Resimleri javascript yolu ile ekleme kismi
+
+const leftSecImg = document.querySelector('.hero-section-left');
+
+const secImgBox = {
+    imgLeft : "./images/left.png",
+
+};
+
+const render ={
+    imgLeft : () =>{
+        leftSecImg.innerHTML = `<img src="${secImgBox.imgLeft}" alt="Left Image">`;
+    },
+};
+
+const heroRender =()=>{
+    render.imgLeft();
+}
+
+heroRender();
