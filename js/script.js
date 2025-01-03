7b7b7bf11a2c99b5396ce7592423ecf7239f0d0a
Emirhan Buğdaycı <148485417+Emirhanbgdyc@users.noreply.github.com>
2025-01-03 22:57:58 +0300

    Fotograflar arasindaki buttonlara ozellestirme geldi

---
+++ js/script.js
@@ -0,0 +1,36 @@
+const leftButton = document.querySelector(".left-btn");
+const rightButton = document.querySelector(".right-btn");
+const productsImg = document.querySelectorAll(".products-area img");
+let idx = 0;
+
+// İlk görseli aktif yap
+productsImg[idx].classList.add("active");
+
+// Sağ düğme (sonraki görsel)
+rightButton.addEventListener("click", () => {
+    idx++;
+    changeImages();
+});
+
+// Sol düğme (önceki görsel)
+leftButton.addEventListener("click", () => {
+    idx--;
+    changeImages();
+});
+
+const changeImages = () => {
+    if (idx > productsImg.length - 1) {
+        idx = 0;
+    } else if (idx < 0) {
+        idx = productsImg.length - 1;
+    }
+
+    productsImg.forEach((img, i) => {
+        img.classList.remove("active", "inactive");
+        if (i === idx) {
+            img.classList.add("active");
+        } else {
+            img.classList.add("inactive");
+        }
+    });
+};
