a7d24d33fbd84a8b14f44d41fd7b677cc8d87a8c
Emirhan Buğdaycı <148485417+Emirhanbgdyc@users.noreply.github.com>
2025-01-20 21:53:04 +0300

    Erkek resimler

--- js/script.js
+++ js/script.js
@@ -37,6 +37,7 @@ const changeImages = () => {
         }
     });
 };
+
 //? ---------Bu kisim careouselin javascript kodlari bitisi ---------------- 
 // HTML'deki butonlar ve ürünler için referansları alıyoruz
 const buttons = document.querySelectorAll(".arrivals__button");
@@ -45,9 +46,12 @@ const productsContainer = document.querySelector(".arrivals__products__grid");
 // Örnek ürün verileri (Her kategori için farklı ürünler)
 const products = {
   "Men's Fashion": [
-    { name: "Men's Shirt", image: "images/arrivals__grid__img/arrivals-img-three.svg", brand: "Al Karam", reviews: "1.2k", price: "49.99", status: "Available" },
-    { name: "Men's Jeans", image: "images/arrivals__grid__img/arrivals-img-three.svg", brand: "Al Karam", reviews: "900", price: "69.99", status: "Available" },
-    { name: "Men's Jeans", image: "images/arrivals__grid__img/arrivals-img-three.svg", brand: "Al Karam", reviews: "900", price: "69.99", status: "Available" },
+    { name: "Men's Shirt white", image: "images/arrivals__grid__img/erkek-one.jpg", brand: "Beymen", reviews: "1.2k", price: "120.99", status: "Available" },
+    { name: "Men's Shirt blue", image: "images/arrivals__grid__img/erkek-2.jpg", brand: "Defacto", reviews: "900", price: "169.99", status: "Available" },
+    { name: "Men's Shirt black", image: "images/arrivals__grid__img/erkek-3.jpg", brand: "LCW", reviews: "900", price: "369.99", status: "Available" },
+    { name: "Men's Shirt green", image: "images/arrivals__grid__img/erkek-4.jpg", brand: "Zara", reviews: "900", price: "269.99", status: "Available" },
+    { name: "Men's Shirt blue white", image: "images/arrivals__grid__img/erkek-5.jpg", brand: "Işıl", reviews: "900", price: "569.99", status: "Available" },
+    { name: "Men's Shirt dark blue", image: "images/arrivals__grid__img/erkek-6.jpg", brand: "Nike", reviews: "900", price: "469.99", status: "Available" },
   ],
   "Women's Fashion": [
     { name: "Women's Dress", image: "images/arrivals__grid__img/arrivals-img-one.svg", brand: "Al Karam", reviews: "2.3k", price: "89.99", status: "Almost Sold Out" },
