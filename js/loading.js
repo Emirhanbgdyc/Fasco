1f5318ed22062d197916eb5b1be9e6163ed410e3
Emirhan Buğdaycı <148485417+Emirhanbgdyc@users.noreply.github.com>
2025-02-28 23:00:13 +0300

    Add loading page and styles, update font imports, and adjust hero image max-width

---
+++ js/loading.js
@@ -0,0 +1,22 @@
+
+    window.onload = function(){
+
+        setTimeout(function(){
+            
+            document.getElementById("#loaderContainer")
+
+            window.location.href ="../pages/sign.html"
+
+        }, 3000);
+
+
+
+
+    }
+
+
+    document.addEventListener("DOMContentLoaded", () => {
+        const text = document.querySelector(".loading__text");
+        text.textContent = "Sayfa Yükleniyor...";
+    });
+  
